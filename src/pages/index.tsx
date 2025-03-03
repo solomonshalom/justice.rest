1cfca2c447322a5a96a32814588301eea6e47e42
Solomon Shalom Lijo <solomonlijo@skiff.com>
2025-03-04 01:45:24 +0530

    Jesus Is King!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,7 +84,6 @@ const IndexPage: React.FC<IndexProps> = ({ stats }) => {
 				Introduction
 			</Text>
 
-			{/* The `Text` wrapper makes sure spaces are consistently sized */}
 			<Text as="div" variant="site-intro" sx={{ display: 'contents' }}>
 			<Heading as="h1" variant="site-intro" sx={{ fontWeight: 'bold' }}>
 				We&rsquo;re JusticeRest, and we craft tools that promote equality, morality and justice 
